776c17e02fd67b98f373a8255e236d85f20eddc5
Gerald Onyango <gerald.onyango@gmail.com>
2020-07-06 16:16:18 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,9 @@ const Index = ({ posts, title, description, ...props }) => {
     <>
       <Layout pageTitle={title} description={description}>
         <h1 className="title">Welcome to this ridiculous page that Alanna wanted.</h1>
-  <img src="https://vignette.wikia.nocookie.net/horrormovies/images/3/3e/HenryBowers.png/revision/latest?cb=20190312134628">
-    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fvariety.com%2F2018%2Ffilm%2Fnews%2Fit-chapter-2-teach-grant-adult-henry-bowers-1202856265%2F&psig=AOvVaw1hSp2mVLxN7nHPSgvD6HpE&ust=1594152748369000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIiQg_e3ueoCFQAAAAAdAAAAABAH">
-    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wattpad.com%2Fstory%2F160637288-henry-bowers-my-love%25F0%259F%2598%258D&psig=AOvVaw1hSp2mVLxN7nHPSgvD6HpE&ust=1594152748369000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIiQg_e3ueoCFQAAAAAdAAAAABAN">
+  <img src="https://vignette.wikia.nocookie.net/horrormovies/images/3/3e/HenryBowers.png/revision/latest?cb=20190312134628" />
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fvariety.com%2F2018%2Ffilm%2Fnews%2Fit-chapter-2-teach-grant-adult-henry-bowers-1202856265%2F&psig=AOvVaw1hSp2mVLxN7nHPSgvD6HpE&ust=1594152748369000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIiQg_e3ueoCFQAAAAAdAAAAABAH" />
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wattpad.com%2Fstory%2F160637288-henry-bowers-my-love%25F0%259F%2598%258D&psig=AOvVaw1hSp2mVLxN7nHPSgvD6HpE&ust=1594152748369000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIiQg_e3ueoCFQAAAAAdAAAAABAN" />
       </Layout>
       <style jsx>{`
         .title {
